ed4000e88e6c0d17ce003ca81bb2a14c122cf5c2
Junior's Signature <chibuzor.john.2018@gmail.com>
2025-11-13 10:54:43 +0100

    Update task.py

--- day1/task.py
+++ day1/task.py
@@ -1,1 +1,7 @@
+# 📝 Description
+# Students will create a program that collects their name, age, school, and favorite subject, and prints a short summary about them.
 
+# ✅ Steps
+# Ask the user for: Name, Age, School, Favorite subject
+# Store them in variables
+# Print a summary using print()
