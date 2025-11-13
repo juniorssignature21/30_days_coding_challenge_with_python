5b17eb4915c399c6dac97182d21bceee971d1fa0
codesignature <codesignature19@gmail.com>
2025-11-14 00:32:53 +0100

    day2

---
+++ day2/main.py
@@ -0,0 +1,57 @@
+# x = 10
+# y = 3
+
+# # Addition operator === +
+# z = x + y
+# print(z)
+# # subtraction operator === -
+# z = x - y
+# print(z)
+# # multiplication operator === *
+# z = x * y
+# print(z)
+# # division operator === /
+# z = x / y
+# print(z)
+# # floor division operator === //
+# z = x // y
+# print(z)
+# # modulus operator === %
+# z = x % y
+# print(z)
+# # exponentiation operator === **
+# z = x ** y
+# print(z)
+
+# print("Welcome to the Arithmetic Operations Program!")
+# num1 = float(input("Enter First Number: "))
+# num2 = float(input("Enter Second Number: "))
+
+# print("Choose an option:\n1)Addition\n2)Subtraction\n3)Division\n4)Multiplication\n5)Exponentiation")
+
+# option = input("")
+
+# if option == "1":
+#     print("Sum:", num1 + num2)
+# elif option == "2":
+#     print("Difference:", num1 - num2)
+# elif option == "3":
+#     print("Division:", num1 / num2)
+# elif option == "4":
+#     print("Product:", num1 * num2)
+# elif option == "5":
+#     print("Exponent:",num1 ** num2)
+# else:
+#     print("Invalid Option")
+
+age = 15
+print(type(age))
+
+age = str(age)
+print(type(age))
+
+# age = float(age)
+# print(type(age))
+
+age = list(age)
+print(type(age))
